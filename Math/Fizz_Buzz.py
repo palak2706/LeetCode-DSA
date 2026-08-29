54c60af6b43275225f4538abff459c244c3fe8df
palak <palakraghuwanshi66410@gmail.com>
2026-08-29 23:15:54 +0530

    Solve Problem 412 - Fizz Buzz

---
+++ Math/Fizz_Buzz.py
@@ -0,0 +1,25 @@
+# LeetCode 412 - Fizz Buzz
+
+# Difficulty: Easy
+# Topic: Math / Simulation
+
+class Solution:
+
+    def fizzBuzz(self, n: int) -> list[str]:
+        answer = []
+
+        for i in range(1, n + 1):
+            if i % 3 == 0 and i % 5 == 0:
+                answer.append("FizzBuzz")
+            elif i % 3 == 0:
+                answer.append("Fizz")
+            elif i % 5 == 0:
+                answer.append("Buzz")
+            else:
+                answer.append(str(i))
+
+        return answer
+
+
+# Time Complexity: O(n)
+# Space Complexity: O(n)
