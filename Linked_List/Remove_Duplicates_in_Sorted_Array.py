18c68437a25a0d044130dabb60f76d9db7413a91
palak <palakraghuwanshi66410@gmail.com>
2026-09-25 23:01:59 +0530

    Add LeetCode 83 Remove Duplicates from Sorted List

---
+++ Linked_List/Remove_Duplicates_in_Sorted_Array.py
@@ -0,0 +1,15 @@
+# LeetCode 83 - Remove Duplicates from Sorted List
+# Difficulty: Easy
+# Topic: Linked List
+
+class Solution:
+    def deleteDuplicates(self, head):
+        current = head
+
+        while current and current.next:
+            if current.val == current.next.val:
+                current.next = current.next.next
+            else:
+                current = current.next
+
+        return head
